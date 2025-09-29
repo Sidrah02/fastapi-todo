ef251d0fd81ccfda600efec799809fe37afc7b5e
Sidrah <sidrahjvd@gmail.com>
2025-09-29 13:12:10 +0300

    initial fastapi todo

---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+# Create Base class for all models
+Base = declarative_base()
+
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
