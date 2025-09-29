471a4b8851b413347bc1b45adcc4785c7829baf4
Sidrah <sidrahjvd@gmail.com>
2025-09-29 10:51:13 +0300

    added todo endpoints

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return{"message:welcome to TODO API"}
+
+@app.get("/hello")
+def say_hello():
+    return{"massage:hello from TODOFASTAPI"}
+
+todos = []
+
+@app.get("/todos")
+def get_todos():
+    return todos
+
+@app.get("/todos")
+def add_todo(item:str)
+    todos.append(item)
+    return{"status":"added", "item":item}
